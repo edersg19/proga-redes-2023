86e6e7194d7f0cf9fc0d7f80cb2bcb31ab1faee3
Eder Gael Saldaña Galván <137082872+edersg19@users.noreply.github.com>
2023-10-02 13:00:44 -0500

    Ejercicio práctico

--- Ejercicios prácticos Módulo 2/u1_prob2.py
+++ Ejercicios prácticos Módulo 2/u1_prob2.py
@@ -2,10 +2,10 @@
 # Fecha: 2/Octubre/2023
 # Descripción: Ejercicio práctico 2 - Servidores
 
-materias = input("Escribe el nombre de la materia: ")
-materias[1]
-materias = input("Escribe el nombre de la materia: ")
-materias[2]
-
-print(materias[1])
-print(materias[2])
+asignaturas = ["Probabilidad y Estadística", "Electrónica para IDC", "Conexión de redes WAN", "Administración de servidores I", "Programación de Redes", "Inglés IV"]
+notas = {}
+for asignatura in asignaturas:
+    nota = float(input(f"Ingresa la nota de la unidad I de {asignatura}: "))
+    notas[asignatura] = nota
+for asignatura, nota in notas.items():
+    print(f"En {asignatura} has sacado {nota}")
