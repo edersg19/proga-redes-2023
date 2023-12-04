a353da2f15687869be353b10d1b1c704ae08764e
Eder Gael Saldaña Galván <137082872+edersg19@users.noreply.github.com>
2023-12-04 09:59:29 -0600

    Códigos del día 1 y 4 de diciembre

---
+++ Unidad 3/Archivos de Python/lab2.8 part2-esalgal.py
@@ -0,0 +1,23 @@
+# Nombre: Eder Gael Saldaña Galván
+# Fecha: 4/Diciembre/2023
+# Laboratorio: 2.8
+
+from ncclient import manager
+import xml.dom.minidom
+
+m = manager.connect(
+         host="10.10.20.48",
+         port=830,
+         username="developer",
+         password="C1sco12345",
+         hostkey_verify=False
+         )
+
+netconf_filter = """
+<filter>
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native" />
+</filter>
+"""
+
+netconf_reply = m.get_config(source="running", filter=netconf_filter)
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
---
+++ Unidad 3/Archivos de Python/lab2.8-esalgal.py
@@ -0,0 +1,27 @@
+# Nombre: Eder Gael Saldaña Galván
+# Fecha: 24/Noviembre/2023
+# Laboratorio: 2.7
+
+from ncclient import manager
+import xml.dom.minidom
+
+m = manager.connect (
+    host = "10.10.20.48",
+    port = 830,
+    username = "developer",
+    password = "C1sco12345",
+    hostkey_verify = False
+)
+
+
+netconf_reply = m.get_config(source="running")
+print( xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml() )
+
+netconf_filter = """
+<filter>
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native" />
+</filter>
+"""
+
+netconf_reply = m.get_config(source="running", filter=netconf_filter)
+print( xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml() )
